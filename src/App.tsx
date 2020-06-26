c0a4bb39b8748c9acee29f2be0401e41faf11b32
pedrolauro15 <pedrolauro15@gmail.com.com>
2020-06-26 18:18:45 -0300

    William first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Routes from "./routes";
 import { GlobalStyle } from "./styles/global";
-import { AuthProvider } from "./contexts/authContext";
+import { AuthProvider } from "./contexts/authContext"; //first commit
 
 function App() {
   return (
